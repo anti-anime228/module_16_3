7fa8a2b549970c8f945ce0946783f250480d4d32
anti-anime228 <qwerty9228@bk.ru>
2024-10-30 11:36:56 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,13 +1,44 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 new_app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
 
 
+@new_app.get("/")
+async def get_main_page() -> dict:
+    return {"message": "Главная страница"}
+# http://127.0.0.1:8000/
+
+
+@new_app.get("/user/admin")
+async def admin_panel() -> dict:
+    return {"message": "Вы вошли как администратор"}
+# http://127.0.0.1:8000/user/admin
+
+
+@new_app.get("/user/{user_id}")
+async def get_user_number(user_id: int = Path(ge=1, le=100, description='Enter User ID', example=1)) -> dict:
+    if str(user_id) in users:
+        return {"message": f"Вы вошли как пользователь № {user_id}"}
+    else:
+        return {"message": f"Пользователя № {user_id} не существует"}
+# http://127.0.0.1:8000/user/1
+
+
+@new_app.get("/user/{username}/{age}")
+async def get_user_info(
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+        age: int = Path(ge=18, le=120, description='Enter age', example=24)
+) -> dict:
+    return {"User": username, "Age": age}
+# http://127.0.0.1:8000/user/Ivanko/19
+
+
 @new_app.get("/users")
 async def get_users() -> dict:
     return users
+
 
 @new_app.post('/user/{username}/{age}')
 async def post_user(username: str, age: str) -> str:
@@ -21,7 +52,9 @@ async def update_user(user_id: int, username: str, age: str) -> str:
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} has been updated"
 
+
 @new_app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> dict:
+async def delete_user(user_id: int) -> str:
+    user_id = str(user_id)
     del users[user_id]
-    return users
+    return f"User {user_id} has been deleted"
